9d6d0a7e92f3ff6c6f4d3825b6234fb818a68dd5
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-22 22:36:27 +0530

    k

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ import logo from "./Images/logo.png";
 
 const Navbar=()=>{
     return <div className='navbar'>
-     <img className="logo-img" src={logo}></img>
+     <img className="logo-img" src={logo} alt="logo-img"></img>
     </div>
 }
 export default Navbar;
